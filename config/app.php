3c1343a942f5b8022c24b1d964b4d43cc8334606
John Riepe <johnriepe@gmail.com>
2016-08-25 08:53:07 -0400

    added trusted proxies

--- config/app.php
+++ config/app.php
@@ -152,6 +152,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Laracasts\Flash\FlashServiceProvider::class,
         Laravel\Socialite\SocialiteServiceProvider::class,
+        Fideloper\Proxy\TrustedProxyServiceProvider::class,
         /*
          * Package Service Providers...
          */
