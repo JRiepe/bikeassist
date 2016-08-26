c53967ed77824aa6d6f171e7cfb6a8b3510e4c61
John Riepe <johnriepe@gmail.com>
2016-08-26 12:52:58 -0400

    fixed broken links

--- resources/views/ride.blade.php
+++ resources/views/ride.blade.php
@@ -11,7 +11,7 @@ My Information
 		    <div class="row">
 		        <div class="col-md-10 col-md-offset-1">
 		            <div class="panel panel-default">
-		                <div class="panel-heading">Chart</div>
+		                <div class="panel-heading">Ride</div>
 
 		                <div class="panel-body">
 		                    Your Application's Landing Page.
--- resources/views/trail.blade.php
+++ resources/views/trail.blade.php
@@ -9,7 +9,7 @@ My Information
 		    <div class="row">
 		        <div class="col-md-10 col-md-offset-1">
 		            <div class="panel panel-default">
-		                <div class="panel-heading">Welcome</div>
+		                <div class="panel-heading">Trail</div>
 
 		                <div class="panel-body">
 		                    Your Application's Landing Page.
