827ab46967c441300d2d7aaba6b0ad48f5ee6e29
John Riepe <johnriepe@gmail.com>
2016-08-26 12:57:26 -0400

    about blade

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,12 +4,27 @@
 My Information
 @stop
 
+
 @section('content')
-	
-	<h2>First Name: {{$first_name}}</h2>
+		<div class="container">
+		    <div class="row">
+		        <div class="col-md-10 col-md-offset-1">
+		            <div class="panel panel-default">
+		                <div class="panel-heading">Welcome</div>
+
+		                <div class="panel-body">
+		                    Your Application's Landing Page.
+		                </div>
+		            </div>
+		        </div>
+		    </div>
+		</div>
+@endsection	
+
+
+	<!--<h2>First Name: {{$first_name}}</h2>
 	<h2>Last Name: {{$last_name}}</h2>
 	<h2>email: <a href="mailto:{{$email_address}}">{{$email_address}}<a></h2>
 	<a href="/"><button type="button" class="btn btn-primary">Home</button></a>
 	<a href="/chart"><button type="button" class="btn btn-primary">Chart</button></a>
-	
-@stop
+	-->
