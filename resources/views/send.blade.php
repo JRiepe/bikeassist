246235baa09d6f93cee85af8da285e918c52d36f
John Riepe <johnriepe@gmail.com>
2016-09-09 13:38:05 -0400

    hoping to fix send mail

--- resources/views/send.blade.php
+++ resources/views/send.blade.php
@@ -1,10 +1,9 @@
-@extends('layouts/app')
+
 
 @section('content')
 		<div class="container">
-<p>Name: {{$title}}</p>
-<p>Email: {{$email}}</p>
-<p>Message: {{$content}}</p><br>
-
+				<p>Name: {{$title}}</p>
+				<p>Email: {{$email}}</p>
+				<p>Message: {{$content}}</p><br>
 		</div>
 @endsection	
