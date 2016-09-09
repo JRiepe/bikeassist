362bb17277d174e3f8906a08b219f0f4bb5b90b8
John Riepe <johnriepe@gmail.com>
2016-09-09 13:40:44 -0400

    hope

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -14,27 +14,22 @@ class ContactController extends Controller
     
     public function create()
     {
-        
         return view('/contact');
     } 
 
     public function send(ContactFormRequest $request)
     {
-    	
     	$title = $request->input('name');
     	$email = $request->input('email');
         $content = $request->input('message');
-    	Mail::send('send', array(
+    	Mail::send('/send', array(
 	        	'title' => $title,
 	            'email' => $email,
 	            'content' => $content),
     		function($message)	{
 		        $message->from('johnriepe@gmail.com');
 		        $message->to('johnriepe@gmail.com', 'Admin')->subject('Contact Us');
-    		}); 
-    		
-    	
-   		return redirect('/contact')
-   		->with('message','Thanks for contacting us!');   
+    		});
+   		return redirect('/contact')->with('message','Thanks for contacting us!');   
 	} // end function store
 } // end ContactController
