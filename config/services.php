0f0ffa18013d9955a6595c3c0fcb2540cedf197c
John Riepe <johnriepe@gmail.com>
2016-09-14 22:11:11 -0400

    changed htaccess

--- config/services.php
+++ config/services.php
@@ -42,7 +42,7 @@ return [
     'facebook' => [
     'client_id' => '547716785437851',
     'client_secret' => '5dcf048c5356ce9460d50377d069feea',
-    'redirect' => 'http://protected-fjord-43510.herokuapp.com/callback'
+    'redirect' => 'https://protected-fjord-43510.herokuapp.com/callback'
 ],
 
 ];
