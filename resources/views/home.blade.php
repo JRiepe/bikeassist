11d0fb4862ffb9cbeea10b491fc9155aa78dfb84
John Riepe <johnriepe@gmail.com>
2016-09-14 21:13:27 -0400

    changed home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -61,7 +61,11 @@
         </div>-->
     </div>
 </div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
+
 <script>
-	$('.carousel').carousel('cycle');
+	$('.carousel').carousel({
+  		interval: 2000
+	});
 </script>
 @endsection
